f6b5d3d0748198e530af49879faa193f1c50296d
Breno Soriani <breno.soriani@gmail.com>
2025-07-03 14:26:54 -0300

    Abrindo a Mesa.

--- src/Controllers/TableSessionsController.ts
+++ src/Controllers/TableSessionsController.ts
@@ -1,8 +1,21 @@
 import { Request, Response, NextFunction } from "express";
+import { knex } from "@/database/knex";
+import { z } from "zod";
 
 export class TableSessionsController {
   create = async (req: Request, res: Response, next: NextFunction) => {
     try {
+      const bodySchema = z.object({
+        table_id: z.number(),
+      });
+
+      const { table_id } = bodySchema.parse(req.body);
+
+      await knex<TableSessionsRepository>("table_sessions").insert({
+        table_id,
+        openned_at: knex.fn.now(),
+      });
+
       res.json({ message: "Table Sessions created successfully" });
     } catch (error) {
       next(error);
