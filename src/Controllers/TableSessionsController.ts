8628620cc0e17ff4bdd134ca46141553ac5516a7
Breno Soriani <breno.soriani@gmail.com>
2025-07-03 14:35:58 -0300

    Listando Sessoes Das Mesas

--- src/Controllers/TableSessionsController.ts
+++ src/Controllers/TableSessionsController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { knex } from "@/database/knex";
+import { AppError } from "@/utils/AppError";
 import { z } from "zod";
 
 export class TableSessionsController {
@@ -11,12 +12,33 @@ export class TableSessionsController {
 
       const { table_id } = bodySchema.parse(req.body);
 
+      const session = await knex<TableSessionsRepository>("tables_sesssions")
+        .where({ table_id })
+        .orderBy("openned_at", "desc")
+        .first();
+
+      if (session && !session.closed_at) {
+        throw new AppError("this table is already open");
+      }
+
       await knex<TableSessionsRepository>("table_sessions").insert({
         table_id,
         openned_at: knex.fn.now(),
       });
 
       res.json({ message: "Table Sessions created successfully" });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  index = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const sessions = await knex<TableSessionsRepository>(
+        "table_sessions"
+      ).orderBy("closed_at");
+
+      res.json(sessions);
     } catch (error) {
       next(error);
     }
--- src/routes/tables-sessions-routes.ts
+++ src/routes/tables-sessions-routes.ts
@@ -6,5 +6,6 @@ const tableSessionsRouters = Router();
 const tableSessionsController = new TableSessionsController();
 
 tableSessionsRouters.post("/", tableSessionsController.create);
+tableSessionsRouters.get("/", tableSessionsController.index);
 
 export { tableSessionsRouters };
